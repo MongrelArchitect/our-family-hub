655ab47a74eeb4a2b60ad88a708f2446af9eb0d7
Sean Eric Thomas <set@seanericthomas.com>
2024-06-14 11:01:25 -0700

    Ensure connection to database

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import pg from "pg";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const checkPg = async () => {
+  const { Client } = pg;
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: "our-family-hub-database-dev-1",
+    database: process.env.POSTGRES_DB,
+    port: 5432,
+  });
+  await client.connect();
+  const res = await client.query(`SELECT * FROM users`);
+  await client.end();
+  return res.rows as User[];
+};
+
+export default async function Home() {
+  const users = await checkPg();
   return (
-    <main className="min-h-screen bg-rose-300">
-      Hello world!
+    <main className="min-h-screen bg-teal-100">
+      {users.map((user) => {
+        return <div key={user.id}>{user.name}</div>;
+      })}
     </main>
   );
 }
