014eb867f5386bdbb7b00bf7c6a6fb612a24f0f1
Sean Eric Thomas <set@seanericthomas.com>
2024-07-02 12:13:17 -0700

    Use parameterized queries

--- src/lib/db/families.ts
+++ src/lib/db/families.ts
@@ -9,12 +9,14 @@ export async function createNewFamily(
     await client.query("BEGIN");
     // create the family with user as admin
     const familyResult = await client.query(
-      `INSERT INTO families (admin_id, surname) VALUES (${userId}, '${surname}') RETURNING id`,
+      "INSERT INTO families (admin_id, surname) VALUES ($1, $2) RETURNING id",
+      [userId, surname],
     );
     const familyId: number = familyResult.rows[0].id;
     // also add them to the junction table tracking family members
     await client.query(
-      `INSERT INTO family_members (family_id, member_id) VALUES (${familyId}, ${userId})`,
+      "INSERT INTO family_members (family_id, member_id) VALUES ($1, $2)",
+      [familyId, userId],
     );
     await client.query("COMMIT");
     // return the id of the newly created family, for redirecting to its page
@@ -30,7 +32,8 @@ export async function createNewFamily(
 export async function checkIfFamilyExists(familyId: number):Promise<boolean> {
   try {
     const res = await pool.query(
-      `SELECT EXISTS(SELECT 1 FROM families WHERE id = ${familyId})`
+      "SELECT EXISTS(SELECT 1 FROM families WHERE id = $1)",
+      [familyId],
     );
     const exists: boolean = res.rows[0].exists;
     return exists;
@@ -42,7 +45,8 @@ export async function checkIfFamilyExists(familyId: number):Promise<boolean> {
 export async function getMemberCount(familyId: number): Promise<number> {
   try {
     const res = await pool.query(
-      `SELECT COUNT(*) FROM family_members WHERE family_id = ${familyId}`,
+      "SELECT COUNT(*) FROM family_members WHERE family_id = $1",
+      [familyId],
     );
     const count: number = +res.rows[0].count;
     return count;
@@ -54,7 +58,8 @@ export async function getMemberCount(familyId: number): Promise<number> {
 export async function getSurname(familyId: number): Promise<string> {
   try {
     const res = await pool.query(
-      `SELECT surname FROM families WHERE id = ${familyId}`,
+      "SELECT surname FROM families WHERE id = $1",
+      [familyId],
     );
     const surname: string = res.rows[0].surname;
     return surname;
--- src/lib/db/users.ts
+++ src/lib/db/users.ts
@@ -8,7 +8,8 @@ export async function addUserToDatabase(user: {
 }): Promise<number> {
   try {
     const result = await pool.query(
-      `INSERT INTO users (name, email, image) VALUES ('${user.name}', '${user.email}', '${user.image}') RETURNING id`,
+      "INSERT INTO users (name, email, image) VALUES ($1, $2, $3) RETURNING id",
+      [user.name, user.email, user.image],
     );
     return result.rows[0].id;
   } catch (err) {
@@ -21,7 +22,8 @@ export async function getUserIdFromEmail(email: string): Promise<number> {
   // will return user id if they exist, or 0 if not
   try {
     const result = await pool.query(
-      `SELECT id FROM users WHERE email = '${email}'`,
+      "SELECT id FROM users WHERE email = $1",
+      [email],
     );
     if (result.rows.length) {
       return result.rows[0].id;
@@ -36,7 +38,8 @@ export async function getUserIdFromEmail(email: string): Promise<number> {
 export async function updateUserLoginTimestamp(email: string) {
   try {
     const result = await pool.query(
-      `UPDATE users SET last_login_at = NOW() WHERE email = '${email}'`,
+      "UPDATE users SET last_login_at = NOW() WHERE email = $1",
+      [email],
     );
     return result;
   } catch (err) {
