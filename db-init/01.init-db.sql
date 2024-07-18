f9b90f5a32838045419e95a3dc8dbc2737d98783
Sean Eric Thomas <set@seanericthomas.com>
2024-07-18 11:57:25 -0700

    Create tables for todo lists

--- db-init/01.init-db.sql
+++ db-init/01.init-db.sql
@@ -31,3 +31,25 @@ CREATE TABLE IF NOT EXISTS invites (
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   PRIMARY KEY (user_id, family_id)
 );
+
+/* todo lists table */
+CREATE TABLE IF NOT EXISTS todo_lists (
+  id SERIAL PRIMARY KEY,
+  family_id INT NOT NULL REFERENCES families(id),
+  created_by INT NOT NULL REFERENCES users(id),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  title TEXT NOT NULL,
+  description TEXT
+);
+
+/* tasks that each todo_list can have */
+CREATE TABLE IF NOT EXISTS tasks (
+  id SERIAL PRIMARY KEY,
+  todo_list_id INT NOT NULL REFERENCES todo_lists(id),
+  created_by INT NOT NULL REFERENCES users(id),
+  assigned_to INT REFERENCES users(id),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  title TEXT NOT NULL,
+  details TEXT,
+  due_by TIMESTAMPTZ
+);
