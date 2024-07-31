a4f4f9c056ee723e13837d15f4eee24ae606ec87
Sean Eric Thomas <set@seanericthomas.com>
2024-07-30 18:01:28 -0700

    Show thread summaries on family page

--- src/app/(root)/families/[familyId]/page.tsx
+++ src/app/(root)/families/[familyId]/page.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import starIcon from "@/assets/icons/star.svg";
+import threadIcon from "@/assets/icons/thread.svg";
+import todoListIcon from "@/assets/icons/todo-list.svg";
 import Card from "@/components/Card";
 import { getFamilyInfo } from "@/lib/db/families";
 import { getUserInfo } from "@/lib/auth/user";
+import { getThreadSummaries } from "@/lib/db/threads";
 import { getTodoListSummaries } from "@/lib/db/todos";
 
 export default async function FamilyPage({
@@ -22,11 +25,12 @@ export default async function FamilyPage({
   const family = await getFamilyInfo(familyId);
   const userIsAdmin = user.id === family.adminId;
   const todoLists = await getTodoListSummaries(familyId);
+  const threads = await getThreadSummaries(familyId);
 
   const showTodoListSummaries = () => {
     if (todoLists.length) {
       return (
-        <ul>
+        <ul className="flex flex-col gap-2">
           {todoLists.map((todoList) => {
             return (
               <li key={`todo-${todoList.id}`}>
@@ -38,6 +42,30 @@ export default async function FamilyPage({
                   {todoList.title}
                 </Link>{" "}
                 ({todoList.taskCount} task{todoList.taskCount === 1 ? "" : "s"})
+              </li>
+            );
+          })}
+        </ul>
+      );
+    }
+    return <p>No todo lists.</p>;
+  };
+
+  const showThreadSummaries = () => {
+    if (threads.length) {
+      return (
+        <ul className="flex flex-col gap-2">
+          {threads.map((thread) => {
+            return (
+              <li key={`thread-${thread.id}`}>
+                <Link
+                  className="font-bold text-violet-800 hover:underline focus:underline"
+                  href={`/families/${familyId}/threads/${thread.id}`}
+                  title={`View "${thread.title}" thread`}
+                >
+                  {thread.title}
+                </Link>{" "}
+                ({thread.postCount} post{thread.postCount === 1 ? "" : "s"})
               </li>
             );
           })}
@@ -64,8 +92,19 @@ export default async function FamilyPage({
         </p>
         <p>Admin: {userIsAdmin ? "You" : family.adminName}</p>
       </Card>
-      <Card heading="Todo Lists" headingColor="bg-emerald-200">
+      <Card
+        flair={<Image alt="" src={todoListIcon} />}
+        heading="Todo Lists"
+        headingColor="bg-emerald-200"
+      >
         {showTodoListSummaries()}
+      </Card>
+      <Card
+        flair={<Image alt="" src={threadIcon} />}
+        heading="Discussion Threads"
+        headingColor="bg-emerald-200"
+      >
+        {showThreadSummaries()}
       </Card>
     </main>
   );
--- src/lib/db/threads.ts
+++ src/lib/db/threads.ts
@@ -50,10 +50,11 @@ export async function createNewThread(familyId: number, formData: FormData) {
   }
 }
 
-export const getThreadInfo = cache(async (threadId: number, familyId: number) => {
-  try {
-    const userId = await getUserId();
-    const query = `
+export const getThreadInfo = cache(
+  async (threadId: number, familyId: number) => {
+    try {
+      const userId = await getUserId();
+      const query = `
       WITH member_check AS (
         SELECT 1
         FROM family_members
@@ -70,27 +71,66 @@ export const getThreadInfo = cache(async (threadId: number, familyId: number) =>
       AND EXISTS(SELECT 1 FROM member_check)
       ;
     `;
-    const result = await pool.query(query, [
-      userId, familyId, threadId
-    ]);
-    if (!result.rowCount) {
-      // rowCount will be 1 if successfully created, 0 if not
-      throw new Error("Error getting thread info");
+      const result = await pool.query(query, [userId, familyId, threadId]);
+      if (!result.rowCount) {
+        // rowCount will be 1 if successfully created, 0 if not
+        throw new Error("Error getting thread info");
+      }
+      const threadInfo: ThreadInterface = {
+        id: +result.rows[0].id as number,
+        authorId: +result.rows[0].author_id as number,
+        familyId: +result.rows[0].family_id as number,
+        title: result.rows[0].title as string,
+        content: result.rows[0].content as string,
+        createdAt: new Date(result.rows[0].created_at as string),
+        postCount: +result.rows[0].post_count,
+      };
+
+      return threadInfo;
+    } catch (err) {
+      // XXX TODO XXX
+      // log this
+      throw err;
     }
-    const threadInfo: ThreadInterface = {
-      id: +result.rows[0].id as number,
-      authorId: +result.rows[0].author_id as number,
-      familyId: +result.rows[0].family_id as number,
-      title: result.rows[0].title as string,
-      content: result.rows[0].content as string,
-      createdAt: new Date(result.rows[0].created_at as string),
-      postCount: +result.rows[0].post_count,
-    };
+  },
+);
 
-    revalidatePath(`/families/${familyId}/`);
-    return threadInfo;
-    // XXX more paths...
-
+export const getThreadSummaries = cache(async (familyId: number) => {
+  try {
+    const userId = await getUserId();
+    // XXX TODO XXX
+    // sort by latest post...or what?
+    const query = `
+      WITH member_check AS (
+        SELECT 1
+        FROM family_members
+        WHERE member_id = $1
+        AND family_id = $2
+      )
+      SELECT t.id, t.author_id, t.family_id, t.title, t.content, t.created_at, 
+      COUNT(p.id) + 1 AS post_count
+      FROM threads t
+      LEFT JOIN posts p
+      ON t.id = p.thread_id
+      GROUP BY t.id
+      HAVING EXISTS(SELECT 1 FROM member_check)
+      ;
+    `;
+    const result = await pool.query(query, [userId, familyId]);
+    const threads: ThreadInterface[] = [];
+    result.rows.forEach((row) => {
+      const thread: ThreadInterface = {
+        id: +row.id as number,
+        authorId: +row.author_id as number,
+        familyId: +row.family_id as number,
+        title: row.title as string,
+        content: row.content as string,
+        createdAt: new Date(row.created_at as string),
+        postCount: +row.post_count,
+      };
+      threads.push(thread);
+    });
+    return threads;
   } catch (err) {
     // XXX TODO XXX
     // log this
