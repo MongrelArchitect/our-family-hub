86b9eaa8924f68c9b95e60f183bf7d58b81f9303
Sean Eric Thomas <set@seanericthomas.com>
2024-09-03 15:18:55 -0700

    Add image when creating new family

--- src/components/ImagePicker.tsx
+++ src/components/ImagePicker.tsx
@@ -1,17 +1,19 @@
 "use client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ProfileImage from "./ProfileImage";
 
 import noImageIcon from "@/assets/icons/image-off-outline.svg";
 
 interface Props {
+  attempted: boolean;
   clearTrigger: boolean;
   forProfile?: boolean;
   id: string;
   removeError?: () => void;
+  required?: true;
   tabIndex?: number;
-  userId: number;
+  userId?: number;
 }
 
 function decimalRound(num: number) {
@@ -38,23 +40,37 @@ function prettyBytes(bytes: number) {
 }
 
 export default function ImagePicker({
+  attempted,
   clearTrigger,
   forProfile,
   id,
   removeError,
+  required,
   tabIndex,
   userId,
 }: Props) {
+  const filePickerRef = useRef<HTMLInputElement>(null);
+
+  const [error, setError] = useState<string | null>(null);
   const [file, setFile] = useState<File | null>(null);
+  const [valid, setValid] = useState(!required);
 
   useEffect(() => {
+    const input = filePickerRef.current;
+    if (input) {
+      input.value = "";
+    }
     setFile(null);
+    setError(null);
+    setValid(!required);
   }, [clearTrigger]);
 
   const displayImageInfo = () => {
     if (file) {
       return (
-        <div className="flex flex-col items-center gap-1">
+        <div
+          className={`${attempted && error ? "text-red-700" : null} flex flex-col items-center gap-1`}
+        >
           <div className="break-all">{file.name}</div>
           <div>{prettyBytes(file.size)}</div>
         </div>
@@ -82,8 +98,30 @@ export default function ImagePicker({
       );
     }
     return (
-      <ProfileImage reloadTrigger={clearTrigger} size={128} userId={userId} />
+      // XXX handle this better now that userId is optional
+      <ProfileImage
+        reloadTrigger={clearTrigger}
+        size={128}
+        userId={userId || 0}
+      />
     );
+  };
+
+  const chooseInvalidFileError = (file: File) => {
+    if (!file.type.includes("image/")) {
+      return "File is not an image";
+    }
+    if (file.size > 21000000) {
+      return "Image is too large (20MB max)";
+    }
+    return null;
+  };
+
+  const checkValidImage = (file: File) => {
+    if (!file.type.includes("image/") || file.size > 21000000) {
+      return false;
+    }
+    return true;
   };
 
   const handleChange = (event: React.SyntheticEvent) => {
@@ -91,6 +129,8 @@ export default function ImagePicker({
     const { files } = target;
     if (files) {
       setFile(files[0]);
+      setError(chooseInvalidFileError(files[0]));
+      setValid(checkValidImage(files[0]));
     }
     removeError ? removeError() : null;
   };
@@ -100,20 +140,30 @@ export default function ImagePicker({
     const { files } = event.dataTransfer;
     if (event.dataTransfer && files[0]) {
       setFile(files[0]);
+      setError(chooseInvalidFileError(files[0]));
+      setValid(checkValidImage(files[0]));
     }
-    const input = document.getElementById(id) as HTMLInputElement;
-    input.files = files;
+    const input = filePickerRef.current;
+    if (input) {
+      input.files = files;
+    }
+
     removeError ? removeError() : null;
   };
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex h-64 flex-col items-center justify-center gap-2 border-2 border-neutral-900">
+      <div
+        className={`${attempted && error ? "border-red-700" : "border-neutral-900"} flex h-64 flex-col items-center justify-center gap-2 border-2`}
+      >
         {forProfile ? displayProfilePreview() : displayImagePreview()}
         <div className="font-mono text-sm">{displayImageInfo()}</div>
+        {attempted && error ? (
+          <div className="text-red-700">{error}</div>
+        ) : null}
       </div>
       <div
-        className="font-mono flex h-32 w-full flex-col items-center justify-center gap-2 border-2 border-dashed border-neutral-900"
+        className="flex h-32 w-full flex-col items-center justify-center gap-2 border-2 border-dashed border-neutral-900 font-mono"
         onDragOver={(e) => {
           e.preventDefault();
         }}
@@ -133,7 +183,15 @@ export default function ImagePicker({
           id={id}
           name={id}
           onChange={handleChange}
+          ref={filePickerRef}
+          required={required}
           type="file"
+        />
+        <input
+          id={`${id}-validity`}
+          name={`${id}-validity`}
+          type="hidden"
+          value={valid ? 1 : 0}
         />
       </div>
     </div>
--- src/lib/images/images.ts
+++ src/lib/images/images.ts
@@ -1,9 +1,53 @@
 "use server";
 
 import sharp from "sharp";
-import {fileTypeFromBlob} from 'file-type';
+import { fileTypeFromBlob } from "file-type";
 
 import getUserId from "../auth/user";
+
+export async function addNewFamilyImage(
+  name: string,
+  familyId: number,
+  formData: FormData,
+) {
+  const file = formData.get(name) as File;
+  const surname = formData.get("surname") as string;
+  const SIZE = 640;
+  if (!surname) {
+    throw new Error("Missing surname");
+  }
+  if (!file.size) {
+    // no image provided, make a default one
+    const firstLetter = surname[0].toUpperCase();
+
+    const svg = `
+    <svg width="${SIZE}" height="${SIZE}" xmlns="http://www.w3.org/2000/svg">
+      <rect width="100%" height="100%" fill="#ddd6fe"/>
+      <text x="${SIZE / 2}" y="${SIZE / 2 + 96}" font-size="320" font-weight="bold" fill="black" dominant-baseline="middle" text-anchor="middle" font-family="Arial">
+        ${firstLetter}
+      </text>
+    </svg>
+  `;
+    await sharp(Buffer.from(svg))
+      .resize(SIZE, SIZE, { fit: "fill" })
+      .webp()
+      .toFile(`./src/uploads/families/${familyId}.webp`);
+  } else {
+    // user gave an image, use it
+    const fileInfo = await fileTypeFromBlob(file);
+    if (!fileInfo || !fileInfo.mime.includes("image")) {
+      throw new Error("File is not an image");
+    }
+    if (file.size > 21000000) {
+      throw new Error("Image is too large (20MB max)");
+    }
+    const buffer = await file.arrayBuffer();
+    await sharp(buffer)
+      .resize(SIZE, SIZE, { fit: "inside" })
+      .webp()
+      .toFile(`./src/uploads/families/${familyId}.webp`);
+  }
+}
 
 export async function updateProfileImage(name: string, formData: FormData) {
   const userId = await getUserId();
